c13cd34e3174318b1c1470b11d5d15dfd7052c94
academicjobs2 <academicjobs2@gmail.com>
2023-11-16 15:00:15 +1100

    a

--- src/views/Sydney.js
+++ src/views/Sydney.js
@@ -25,9 +25,9 @@ const Sydney = () => {
 
     <div className="">
       <Helmet>
-        <title>PhD Jobs</title>
-        <meta name="description" content="Discover flexible and fulfilling on-campus employment opportunities for Ph.D. candidates. Explore a myriad of PhD positions across diverse departments at your university. Take the next step in your career, apply today, and cultivate valuable work experience and skills." />
-        <meta name="keywords" content="PhD Jobs , PhD academic Jobs" />
+        <title>Sydney | Apply Online - Job Vacancies</title>
+        <meta name="description" content="Are you searching for a teaching job in the Sydney? Visit our website at Teaching Jobs, to see our available employment positions for teaching jobs in Sydney" />
+        <meta name="keywords" content="teaching Jobs Sydney, teaching positions Sydney" />
       </Helmet>
       <div className="blurb text-left py-8 ">
         <h1 className=" font-bold text-lg md:text-2xl py-4 px-7 bg-[#f4a10c] text-white rounded-full shadow-xl ">Graduate Jobs</h1>
@@ -39,7 +39,7 @@ const Sydney = () => {
         <div className="newLine">
           <div className="py-4 px-7 rounded-2xl ">
 
-            <p >PhD jobs on campus involve working part-time or casually in various roles and departments within a university or research institute setting. These jobs provide PhD graduates with the opportunity to earn income, gain valuable work experience, develop employability skills, and enhance their academic and social networks. PhD jobs on campus can cover administration, customer service, events, marketing, IT, library, research, teaching, and other roles. You can find hundreds of job postings from universities across Australia and around the world. Gain valuable work experience and skills by applying today!  </p>
+            <p >The New South Wales Education Standards Authority (NESA), an independent statutory body distinct from any educational authority, is where one must register in order to teach in the state. Teachers in all New South Wales schools and colleges are registered with the NESA. Teachers who are currently registered in other states or territories may be eligible for mutual recognition. We hope that this information may be useful to you as you look for Sydney teaching opportunities. Best of luck! </p>
           </div>
           <div className="newLine mb-2"></div>
    
