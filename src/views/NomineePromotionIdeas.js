ffafbd589383d64bed4d63b870da5789ad3c5557
Dan Grant <dan@zygadox.com>
2023-11-24 10:38:18 +1100

    Update principal resources page

--- src/views/NomineePromotionIdeas.js
+++ src/views/NomineePromotionIdeas.js
@@ -22,53 +22,222 @@ const NomineePromoIdeas = () => {
         <h1 className="text-5xl font-semibold  text-[#f4a10c] ">
           Principal of the Year Resources!
         </h1>
-        <h3 className="text-lg mt-4 pb-8">Nominate your Principal NOW!</h3>
 
-        <h4 className="text-lg text-[#f4a10c]">
-          To nominate an outstanding principal for the auspicious{" "}
-          <span className="poty">"PRINCIPAL OF THE YEAR 2023 AWARD"</span>{" "}
-          simply complete the form below:{" "}
-        </h4>
-
-        <div className="p-4 bg-gray-100 mt-[3rem] hidden">
-          <p className="mb-4 text-lg font-semibold text-[#f4a10c]">
-            Drop us an email:
+        <h2 className="text-sky-500 mt-10 text-xl mt-20">
+          Website Announcement Helper Template (between red lines)
+        </h2>
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
+        {/* Website Announcement Helper Template START */}
+        <section className="template">
+          <h2 class="header">Principal of the Year 2023 Nominee</h2>
+          <h4 class="sub-header">Celebrating Excellence in Leadership</h4>
+          <div className="school-images">
+            <span>
+              <img
+                src="/placeholders/principal-headshot.png"
+                alt="[Principal's Name]"
+                className="principal-headshot"
+              />
+            </span>
+            <span>
+              <img
+                src="/placeholders/your-school-logo.png"
+                alt="[School's Name]"
+                className="school-logo"
+              />
+            </span>
+          </div>
+          <strong></strong>
+          <p>We are Proud to Announce:</p>
+          <p class="principal-name">
+            Our esteemed Principal, <strong>[Principal’s Name]</strong>, has
+            been nominated for the prestigious 'Principal of the Year 2023'
+            award. This nomination recognizes their unwavering commitment to
+            excellence in education and inspirational leadership at{" "}
+            <strong>[School’s Name]</strong>.
           </p>
-          <ul className="list-disc ml-6 space-y-2">
+
+          <p class="principal-name">
+            <strong>[Principal’s Name]</strong>: A Beacon of Educational
+            Leadership
+          </p>
+          <p>Join Us in Celebrating this Honour</p>
+          <p>
+            We invite our school community to join us in celebrating this
+            significant achievement. <strong>[Principal’s Name]</strong>'s
+            nomination is not just a personal accolade but a testament to our
+            collective dedication to creating an enriching and supportive
+            educational environment.
+          </p>
+          <h4>Stay Informed</h4>
+          <p>
+            For more details about the 'Principal of the Year 2023' award and
+            the nomination, visit{" "}
+            <a
+              target="_blank"
+              href="https://www.teachingjobs.com.au/principal-of-the-year-2023-nominations"
+              rel="noreferrer"
+            >
+              www.TeachingJobs.com.au
+            </a>{" "}
+            to find out more about this auspicious award.
+          </p>
+
+          <img
+            width={140}
+            src="/principal-of-the-year/principal-of-the-year-nominee-badge.png"
+            alt="Digital Nomination Badge"
+            class="nomination-badge"
+          />
+        </section>
+        {/* Website Announcement Helper Template END */}
+
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
+
+        <h2 className="text-sky-500 mt-10 text-xl mt-20">
+          Intranet Announcement Helper Template (between red lines)
+        </h2>
+
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
+        <section className="template">
+          <h2>
+            Subject: Celebrating Our Leader: <strong>[Principal’s Name]</strong>{" "}
+            Nominated for Principal of the Year 2023
+          </h2>
+          <p>&nbsp;</p>
+          <p>
+            Dear <strong>[School Name]</strong> Community,
+          </p>
+          <p>
+            We have some incredibly exciting news to share! Our esteemed
+            principal, <strong>[Principal’s Name]</strong>, has been nominated
+            for the auspicious 'Principal of the Year 2023' award. This
+            nomination recognizes
+            <strong>[Principal’s Name]’s</strong> exceptional leadership,
+            dedication to educational excellence, and unwavering commitment to
+            our school community.
+          </p>
+          <h2>Why This Nomination Matters:</h2>
+          <ul>
             <li>
-              <span className="font-semibold">
-                University and College Advertisers:
-              </span>{" "}
-              BestResults (AT) Teachingjobs.com
+              <strong>Leadership:</strong> <strong>[Principal’s Name]</strong>{" "}
+              has been an inspirational figure, guiding both our staff and
+              students towards achieving remarkable goals.
             </li>
             <li>
-              <span className="font-semibold">
-                Teachings and Higher Ed Staff:
-              </span>{" "}
-              TheBestJobs (AT) TeachingJobs.com
+              <strong>Innovation:</strong> Under their leadership, [*** mention
+              any notable initiatives or programs ***].
             </li>
             <li>
-              <span className="font-semibold">Student Jobs on Campus:</span>{" "}
-              StudentWork (AT) TeachingJobs.com
-            </li>
-            <li>
-              <span className="font-semibold">Teaching Talent Pool:</span>{" "}
-              TalentPool (AT) AcadmeicJobs.com
+              <strong>Community Impact:</strong> [*** Describe how the principal
+              has positively impacted the school community ***].
             </li>
           </ul>
-          <p className="mt-4">Or call us:</p>
-        </div>
+          <p>Let’s Show Our Support:</p>
+          <ul>
+            <li>
+              Spread the Word: Share this news with your peers and colleagues.
+            </li>
+            <li>
+              Engage on Social Media: Follow our school's social media channels
+              and join in the conversation using #PrincipalOfTheYear2023.
+            </li>
+            <li>
+              Share Your Stories: We would love to hear your personal
+              experiences and stories about <strong>[Principal’s Name]</strong>.
+              Please submit them [*** provide method or link ***].
+            </li>
+          </ul>
+          <p>
+            This nomination is not just a personal achievement for{" "}
+            <strong>[Principal’s Name]</strong> but a moment of pride for our
+            entire [School Name] family. Let’s come together to support and
+            celebrate <strong>[Principal’s Name]’s</strong> journey in this
+            prestigious award.
+          </p>
+          <p>
+            To learn more about the auspicious 'Principal of the Year 2023'
+            award and the nomination, visit{" "}
+            <a
+              target="_blank"
+              href="https://www.teachingjobs.com.au/principal-of-the-year-2023-nominations"
+              rel="noreferrer"
+            >
+              www.TeachingJobs.com.au
+            </a>
+            .
+          </p>
+          <p>Thank you for being part of this momentous occasion.</p>
+          <p>Warm regards,</p>
+          <p>[Your Name or Communication Team's Name]</p>
+          <p>[Your Position or Team's Position]</p>
+          <p>[School Name]</p>
+        </section>
 
-        <Link to="https://iloveTeachingjobs.com/Nominate/">
-          <img
-            src="https://Teachingjobs.s3.amazonaws.com/img/_misc/Nominate-us.jpeg"
-            alt="Nominate us"
-            className="mt-[4rem] hidden"
-          />
-        </Link>
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
+
+        <h2 className="text-sky-500 mt-10 text-xl mt-20">
+          Email Announcement Helper Template (between red lines)
+        </h2>
+
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
+
+        <section className="template">
+          <h2 style={{ color: "#4A90E2" }}>
+            Subject: Proudly Nominated for Principal of the Year 2023!
+          </h2>
+          <p>&nbsp;</p>
+          <p>Dear [School Community/Colleagues/Parents/Students],</p>
+          <p>
+            I am excited to share some wonderful news with you all. I have been
+            nominated for the 'Principal of the Year 2023' award, a significant
+            recognition of our collective efforts in fostering an environment of
+            excellence in education.
+          </p>
+          <p>
+            This nomination is not just a personal honor but a testament to the
+            hard work, dedication, and spirit of our entire school community. It
+            reflects our shared commitment to creating a nurturing and inspiring
+            educational environment.
+          </p>
+          <h2 style={{ color: "#4A90E2" }}>
+            Here's How You Can Join in the Celebration:
+          </h2>
+          <ul>
+            <li>
+              Visit Our School Website: Read more about the nomination and what
+              it means for our school.
+            </li>
+            <li>
+              Stay Tuned on Social Media: Follow our school's social media
+              channels for updates and share the news with your networks using
+              #PrincipalOfTheYear2023 and #EducationalExcellence.
+            </li>
+            <li>
+              Spread the Word: Let’s celebrate this moment together! Feel free
+              to share this news with friends and family.
+            </li>
+          </ul>
+          <p>
+            I am truly humbled by this nomination and want to extend my
+            heartfelt gratitude to each one of you for your continuous support
+            and enthusiasm. Together, we make our school a wonderful place to
+            learn and grow.
+          </p>
+          <p>Thank you for being an integral part of this journey.</p>
+          <p>Warm regards,</p>
+          <p>[Your Name]</p>
+          <p>[Your Title]</p>
+          <p>[Your School]</p>
+        </section>
+        {/* Divider */}
+        <div className="pt-6 border-b-2 border-red-500 mb-8"></div>
       </div>
-
-      <section className="px-7 py-8 container mx-auto py-16 bg-slate-100 p-4 pt-4 rounded-3xl mt-8 mb-8"></section>
     </div>
   );
 };
