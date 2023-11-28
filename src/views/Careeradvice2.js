98e63576cccea6e8246268a1d48db23771bebe6d
JamieRuskin <152110721+JamieRuskin@users.noreply.github.com>
2023-11-28 13:48:36 +1100

    career advice fix

--- src/views/Careeradvice2.js
+++ src/views/Careeradvice2.js
@@ -34,9 +34,9 @@ const Careeradvice2 = () => {
           </ul>
         </div>
       </div>
-      <h1 className=" font-bold text-xl md:text-xl px-7 text-[#f4ac10] "> Career Advices</h1>
+      <h1 className=" font-bold text-xl md:text-xl px-7 text-[#f4ac10] "> Career Advices11</h1>
       <hr className="w-ful  mx-auto  bg-[#f4ac10] border-8 rounded-full md:my-2 md:mb-4  border-[#f4a10c] shadow-xl "></hr>
-      <div className="mt-[-3.3rem] "><SearchResults q={{ l: "Career_Hub" }} /></div>
+     <div className="mt-[-3.3rem] "><SearchResults q={{ l: "Career_Hub" }} /></div> 
     </div>
   );
 };
